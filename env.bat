2cfb7e5dc391a020d87c05af01bbee562250021e
Sophie627 <alekseizukovski@gmail.com>
2020-08-19 18:08:31 +0300

    Add env files

--- env.bat
+++ env.bat
@@ -1,11 +1,2 @@
-git add .
-git commit -m %1
-git push origin master
-start /B /wait flutter build web
-echo 1
-cd /D build/web
-git add .
-git commit -m %1
-git push origin master
-cd /D ../..
-
+start env1 %1
+start env2 %1
---
+++ env2.bat
@@ -0,0 +1,6 @@
+cd /D build/web
+git add .
+git commit -m %1
+git push origin master
+cd /D ../..
+
