99cfe83553d1709fb3df0a2162b97349d8f161d8
Sophie627 <alekseizukovski@gmail.com>
2020-08-20 19:21:13 +0300

    Changing title on flip page

--- lib/screen/flippageScreen.dart
+++ lib/screen/flippageScreen.dart
@@ -103,7 +103,7 @@ class _FlipPageScreenState extends State<FlipPageScreen> {
 </head>
 <body>
     <div id="flipbook" style="margin:200px auto;">
-        <div class="hard">Cover</div>
+        <div class="hard">Sophie's Story</div>
         <div class="hard"></div>
         <div class="page"><img style="width:100%;height:100%;" src="https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/01.jpg" draggable="false" alt="" /></div>
         <div class="page"><img style="width:100%;height:100%;" src="https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/02.jpg" draggable="false" alt="" /></div>
